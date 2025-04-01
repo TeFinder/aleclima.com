4240a5961eb96ca8d1f836ebd50edefcab9f8522
Alec Lima <aleclimadev@gmail.com>
2025-03-31 21:43:15 -0300

    chore: add route for components

--- components/footer.vue
+++ components/footer.vue
@@ -3,19 +3,19 @@ const currentYear = computed(() => new Date().getFullYear())
 </script>
 
 <template>
-  <footer class="dark:bg-black bg-white ">
-    <div class="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40 container mx-auto px-4 py-8">
+  <footer class="dark:bg-black bg-[#FAFAFA] ">
+    <div class=" border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40 container mx-auto px-4 py-8">
       <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
         <div class="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-          <NavLink href="/">
+          <NuxtLink href="/">
             Início
-          </NavLink>
-          <NavLink href="/projetos">
+          </NuxtLink>
+          <NuxtLink href="/projetos">
             Projetos
-          </NavLink>
-          <NavLink href="/artigos">
+          </NuxtLink>
+          <NuxtLink href="/artigos">
             Artigos
-          </NavLink>
+          </NuxtLink>
         </div>
         <p class="text-center text-xs text-zinc-600 dark:text-zinc-400">
           &copy; {{ currentYear }} Alec Lima. Todos os direitos reservados.
