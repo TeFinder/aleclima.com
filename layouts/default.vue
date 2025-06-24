4ee23221034639dc47b61d5800a6f9b89cdda4a9
Alec Lima <aleclimadev@gmail.com>
2025-06-23 21:02:20 -0300

    chore: update component default

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,27 +20,38 @@ useSeoMeta({
   <div class="min-h-screen bg-[#FAFAFA] dark:bg-[#181D27] text-zinc-900 dark:text-zinc-50">
     <section class="flex justify-evenly items-center mx-4">
       <div class="flex">
-        <h1 class="text-2xl font-bold dark:text-green-500 text-green-300">@Alec</h1>
+        <NuxtLink
+          to="/"
+          class="text-2xl font-bold bg-gradient-to-r from-teal-300 to-indigo-600 bg-clip-text text-transparent"
+        >
+          @Alec
+        </NuxtLink>
       </div>
       <header class="border-zinc-800">
         <div class="container px-4">
           <nav class="py-6">
             <ul class="flex items-center gap-8 font-semibold">
               <li>
-                <NuxtLink to="/" class="text-lg hover:text-emerald-400 transition-colors duration-200"
-                  :class="{ 'text-emerald-400': route.path === '/' }">
+                <NuxtLink
+                  to="/" class="text-lg hover:text-emerald-400 transition-colors duration-200"
+                  :class="{ 'text-emerald-400': route.path === '/' }"
+                >
                   Inicio
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink to="/artigos" class="text-lg hover:text-emerald-400 transition-colors duration-200"
-                  :class="{ 'text-emerald-400': route.path === '/artigos' }">
+                <NuxtLink
+                  to="/artigos" class="text-lg hover:text-emerald-400 transition-colors duration-200"
+                  :class="{ 'text-emerald-400': route.path === '/artigos' }"
+                >
                   Artigos
                 </NuxtLink>
               </li>
               <li>
-                <NuxtLink to="/projetos" class="text-lg hover:text-emerald-400 transition-colors duration-200"
-                  :class="{ 'text-emerald-400': route.path === '/projetos' }">
+                <NuxtLink
+                  to="/projetos" class="text-lg hover:text-emerald-400 transition-colors duration-200"
+                  :class="{ 'text-emerald-400': route.path === '/projetos' }"
+                >
                   Projetos
                 </NuxtLink>
               </li>
@@ -52,11 +63,9 @@ useSeoMeta({
         <ColorMode />
       </div>
     </section>
-
     <main class="container mx-auto px-4 py-8">
       <slot />
     </main>
   </div>
-
   <Footer />
 </template>
