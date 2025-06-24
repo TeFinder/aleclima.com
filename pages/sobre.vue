57006171043dc4355882b062cb4ede289c93520a
Alec Lima <aleclimadev@gmail.com>
2025-06-23 21:07:14 -0300

    feat: add page about me

---
+++ pages/sobre.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container flex items-center flex-col h-[40vh] justify-center">
+    <h1 class="font-bold text-4xl text-gray-800 mb-8 font-sans bg-gradient-to-r from-teal-300 to-indigo-600 bg-clip-text text-transparent">
+      SOBRE MIM
+    </h1>
+    <p class="text-center mt-4 text-xl">
+      Meu nome é Alec Lima e Sou Programador. Sou o fundador e CEO da TeFinder, onde desenvolvi meios para divulgar vagas de tecnologias e conectar recrutadores e profissionais da tecnologia.
+    </p>
+    <div class="flex justify-center gap-4 mt-8">
+      <Icon name="bi:linkedin" class="size-6" />
+      <a href="https://github.com/AlecLimaDev" target="_blank">
+        <Icon name="bi:github" class="size-6" />
+      </a>
+    </div>
+  </section>
+</template>
