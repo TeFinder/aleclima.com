080cf90e43b62fa31cf94bbe43217e5a81130dee
Alec Lima <aleclimadev@gmail.com>
2025-06-23 21:04:24 -0300

    chore: create component budget, button and hatsapp

---
+++ components/budget.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-[30vh] mt-14 w-full">
+    <h2 class="font-bold text-3xl text-black bg-gradient-to-r from-teal-300 to-indigo-600 bg-clip-text text-transparent">
+      Vamos conversar sobre o seu projeto?
+    </h2>
+    <p class="w-[580px]">
+      Me conta o que você está precisando. Posso te ajudar a transformar uma ideia ou um processo travado em um sistema funcional, seguro e fácil de usar.
+    </p>
+    <Button />
+  </div>
+</template>
---
+++ components/button.vue
@@ -0,0 +1,7 @@
+<template>
+  <button class="text-1xl mt-8 p-3 border-2 border-solid border-green-300 rounded-full text-[#091310] dark:text-primary">
+    <NuxtLink to="/orcamento">
+      <slot>Solicitar Orçamento</slot>
+    </NuxtLink>
+  </button>
+</template>
