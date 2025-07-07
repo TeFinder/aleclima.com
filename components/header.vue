9c0427d03b17aa63680b5ac44276f200db2a6f31
Alec Lima <aleclimadev@gmail.com>
2025-07-07 19:54:20 -0300

    chore: darkmode header

--- components/header.vue
+++ components/header.vue
@@ -15,7 +15,7 @@ function closeMobileMenu() {
 </script>
 
 <template>
-  <header class="bg-zinc-50 font-poppins">
+  <header class="bg-zinc-50 font-poppins dark:bg-[#181D27]">
     <nav aria-label="Top" class="mx-auto max-w-6xl flex items-center justify-between py-6 px-4">
       <div class="flex lg:hidden">
         <button
@@ -23,7 +23,6 @@ function closeMobileMenu() {
           class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-zinc-700"
           @click="mobileMenuOpen = true"
         >
-          <span class="sr-only">Abrir menu</span>
           <svg class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
